a34ee133771b7472188b290238597720d65757a0
pfncl <177448622+pfncl@users.noreply.github.com>
2026-02-15 09:30:18 +0100

    feat(project): initial Astro setup
    - Scaffolded Astro project structure
    - Integrated Svelte framework
    - Configured Cloudflare adapter for deployment
    - Added VS Code settings and launch config
    - Included basic .gitignore and tsconfig

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'hybrid',
+  site: 'https://sklenarstviruska.cz',
+  integrations: [svelte()],
+  adapter: cloudflare(),
+});
